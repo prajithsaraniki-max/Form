fe6fb35c71fb190420a171d9aacb9e416c8d0350
saran <prajithsaraniki@gmail.com>
2025-12-24 13:11:49 +0100

    first commit

---
+++ Server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/students");
+
+const Student = mongoose.model("Student", {
+  name: String,
+  rollno: Number
+});
+
+app.get("/students", async (req, res) => {
+  const students = await Student.find();
+  res.json(students);
+});
+
+app.post("/students", async (req, res) => {
+  const student = new Student(req.body);
+  await student.save();
+  res.json(student);
+});
+
+app.delete("/students/:id", async (req, res) => {
+  await Student.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+app.listen(5000, () => console.log("Backend running on 5000"));
